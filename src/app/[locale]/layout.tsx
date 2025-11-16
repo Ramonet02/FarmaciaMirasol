8b2b935ebbb4e4feab74a126d2a04cdebd05c4d2
Ramonet02 <ramonperez22400@gmail.com>
2025-11-16 22:27:22 +0100

    Changes problems locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,11 +26,13 @@ export const metadata = {
 
 export default async function LocaleLayout({
   children,
-  params: { locale }
+  params
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
+  const { locale } = await params;
+
   if (!routing.locales.includes(locale as any)) {
     notFound();
   }
